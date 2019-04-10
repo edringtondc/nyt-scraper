5f4f98207b0ef22a5f3b6a18e8ebff4a40112d22
katieburrows <katie_burrows@me.com>
2019-04-10 12:38:56 -0600

    got articles to filter properly based on number of results requested, got url built with optional years

--- logic.js
+++ logic.js
@@ -1,51 +1,72 @@
 $(document).ready(function () {
 
     console.log("hi bishhhh")
-    // SUBMIT - Click event
-    //grab text field values
-    //store in variables
-    //create URL --- call function
-    //Run ajax function
-    //empty out the fields
+  
 
     $("#searchBtn").on("click", event => {
         event.preventDefault();
+        $("#results > tbody").empty();
+        
         let searchTermData = $("#searchTerm").val();
         let numRecordsData = $("#numRec").val();
         let startYearData = $("#startYear").val()
         let endYearData = $("#endYear").val()
 
+        $("#searchTerm").val("");
+        $("#numRec").val("1");
+        $("#startYear").val("");
+        $("#endYear").val("");
 
-        createURL(searchTermData)
+        createURL(searchTermData, startYearData, endYearData);
 
         console.log(`searching ${searchTermData} ${numRecordsData} ${startYearData} ${endYearData}`);
 
 
     });
+    $("#clearBtn").on("click", event => {
+        event.preventDefault();
+        $("#results > tbody").empty();
+    });
 
-    const createURL = (searchTermData) => {
-        let URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTermData}&api-key=VpZUli6fofAqZfGkGvU4883LHQZShlgE`
-        console.log(URL)
-        getArticles(URL)
+    const createURL = (searchTermData, beginDate, endDate) => {
+        let baseURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTermData}`;
+        let apiKey = `&api-key=VpZUli6fofAqZfGkGvU4883LHQZShlgE`;
+
+        if (beginDate){
+            baseURL + `&begin_date=${beginDate}0101`;
+      
+        }
+
+        if (endDate){
+            baseURL +`&end_date=${endDate}1231`
+        }
+
+        let fullURL = baseURL + apiKey;
+        
+        
+
+
+
+       
+        console.log(fullURL);
+        getArticles(fullURL);
     }
 
 
 
-    //CLEAR RESULTS- Click event
-    //Empty out the results div
 
 
-    //AJAX function
+
+  
 
     const getArticles = (URL) => {
         $.ajax({
             url: URL,
-            method: "GET"
-
+            method: "GET",
         }).then(function (res) {
             let data = res.response.docs
             console.log(res)
-
+           
             renderResults(data);
 
         })
@@ -69,8 +90,5 @@ $(document).ready(function () {
             $("#results > tbody").append(newTR);
         }
     }
-    //GET
-    //THEN
-    //Take json and return to front end
-    //display to DOM
+
 })
